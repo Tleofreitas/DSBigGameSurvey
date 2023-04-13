0555fa7a8951c0c2265c20c3077f3d71f1ba753c
Thiago <thiagoleonardo1995@live.com>
2023-04-13 17:40:39 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperiorTF/dspesquisaTF/DspesquisaTfApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorTF.dspesquisaTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisaTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisaTfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperiorTF/dspesquisaTF/DspesquisaTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorTF.dspesquisaTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaTfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
