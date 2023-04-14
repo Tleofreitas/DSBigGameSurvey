fdc650bb4fff98d4a80ae668570750e17135fab0
Thiago <thiagoleonardo1995@live.com>
2023-04-14 12:10:42 -0300

    GET Records

--- backend/src/main/java/com/devsuperiorTF/dspesquisaTF/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperiorTF/dspesquisaTF/repositories/RecordRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperiorTF.dspesquisaTF.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperiorTF.dspesquisaTF.entities.Record;
@@ -8,5 +13,10 @@ import com.devsuperiorTF.dspesquisaTF.entities.Record;
 // Operações de acesso a dados
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
+
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min IS NULL) OR obj.moment >= :min) AND "
+			+ "(coalesce(:max IS NULL) OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }
